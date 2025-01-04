c97b80fbc98a6c8d0b9859d9b4e266bb5fd08284
Kroos <1802998976@qq.com>
2025-01-04 22:13:35 +0800

    cross domain

--- src/Router/mod.rs
+++ src/Router/mod.rs
@@ -2,3 +2,7 @@ pub mod Image_routers;
 pub mod User_routers;
 pub mod onchain_router;
 pub mod routers;
+
+use rocket_cors::{AllowedHeaders, AllowedOrigins, Cors, CorsOptions};
+
+
--- src/main.rs
+++ src/main.rs
@@ -21,6 +21,29 @@ use BlockchainImageService::DataBase::*;
 use BlockchainImageService::Router::onchain_router::*;
 use BlockchainImageService::Router::Image_routers::*;
 use BlockchainImageService::Router::User_routers::*;
+use rocket::fairing::{Fairing, Info, Kind};
+use rocket::{Request, Response};
+use rocket::http::Header;
+use async_trait::async_trait;
+
+pub struct CORS;
+
+#[async_trait]
+impl Fairing for CORS {
+    fn info(&self) -> Info {
+        Info {
+            name: "Add CORS headers to responses",
+            kind: Kind::Response,
+        }
+    }
+
+    async fn on_response<'r>(&self, _request: &'r Request<'_>, response: &mut Response<'r>) {
+        response.set_header(Header::new("Access-Control-Allow-Origin", "*"));
+        response.set_header(Header::new("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE"));
+        // 这里你可以根据需求进行更精细化的控制
+        response.set_header(Header::new("Access-Control-Allow-Headers", "Content-Type, Authorization"));
+    }
+}
 
 #[macro_use]
 extern crate rocket;
@@ -33,7 +56,10 @@ fn index() -> &'static str {
 #[launch]
 async fn rocket() -> _ {
     // let rb = get_db().await;
-    rocket::build().mount(
+
+    rocket::build()
+        .attach(CORS)
+        .mount(
         "/",
         routes![
             login,
@@ -46,3 +72,5 @@ async fn rocket() -> _ {
     )
     // .manage(rb)
 }
+
+
